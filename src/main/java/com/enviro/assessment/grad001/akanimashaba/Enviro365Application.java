e1f7a3a8f87fc57316c89a90a717744cd657b6a5
akani <akanimmashaba@gmail.com>
2024-01-26 21:39:24 +0200

    first commit

---
+++ src/main/java/com/enviro/assessment/grad001/akanimashaba/Enviro365Application.java
@@ -0,0 +1,13 @@
+package com.enviro.assessment.grad001.akanimashaba;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Enviro365Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Enviro365Application.class, args);
+	}
+
+}
